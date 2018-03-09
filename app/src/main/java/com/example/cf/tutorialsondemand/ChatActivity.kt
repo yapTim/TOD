c52a4e1fdad44a6c2f5b551474dac234f8479554
Robert Timothy Yap <yap.rtim@gmail.com>
2018-03-09 19:52:18 +0800

    finished chat module

--- app/src/main/java/com/example/cf/tutorialsondemand/ChatActivity.kt
+++ app/src/main/java/com/example/cf/tutorialsondemand/ChatActivity.kt
@@ -1,8 +1,10 @@
 package com.example.cf.tutorialsondemand
 
 import android.content.Context
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
@@ -10,6 +12,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
+import com.example.cf.tutorialsondemand.adapter.SignalAdapter
 import com.example.cf.tutorialsondemand.models.Opentok
 import com.example.cf.tutorialsondemand.models.SignalMessage
 import com.example.cf.tutorialsondemand.retrofit.Connect
@@ -29,9 +32,13 @@ class ChatActivity : AppCompatActivity(), Session.SessionListener, Session.Signa
     val editText = findViewById<EditText>(R.id.message_edit_text)
     val listText = findViewById<ListView>(R.id.message_history_list_view)
 
+    val messageHistory = SignalAdapter(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        listText.adapter = messageHistory
 
         editText.setOnKeyListener(View.OnKeyListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
@@ -62,42 +69,6 @@ class ChatActivity : AppCompatActivity(), Session.SessionListener, Session.Signa
         })
     }
 
-    override fun onPause() {
-        Log.d(logTag, "onPause")
-        super.onPause()
-        session.onPause()
-    }
-
-    override fun onResume() {
-        Log.d(logTag, "onResume")
-        super.onResume()
-        session.onResume()
-    }
-
-    override fun onConnected(session: Session?) {
-
-    }
-
-    override fun onDisconnected(session: Session?) {
-
-    }
-
-    override fun onError(session: Session?, error: OpentokError?) {
-
-    }
-
-    override fun onSignalReceived(session: Session?, type: String?, data: String?, connection: Connection?) {
-
-    }
-
-    override fun onStreamDropped(session: Session?, stream: Stream?) {
-
-    }
-
-    override fun onStreamReceived(session: Session?, stream: Stream?) {
-
-    }
-
     fun initializeSession(apiKey: String, sessionId: String, accessToken: String) {
         session = Session.Builder(this@ChatActivity, apiKey, sessionId).build()
         session.setSessionListener(this@ChatActivity)
@@ -115,5 +86,79 @@ class ChatActivity : AppCompatActivity(), Session.SessionListener, Session.Signa
 
     private fun showMessage(messageData: String, remote: Boolean) {
         Log.d(logTag, "Show Message")
+
+        val message = SignalMessage(messageData, remote)
+        messageHistory.add(message)
     }
+
+    fun logOpentokError(opentokError: OpentokError) {
+        Log.e(logTag, "Error Domain: ${opentokError.errorDomain.name}")
+        Log.e(logTag, "Error Code: ${opentokError.errorCode.name}")
+    }
+
+
+    // alert dialog
+
+    fun showConfigError(alertTitle: String, errorMessage: String) {
+        Log.e(logTag, "Error $alertTitle: $errorMessage")
+
+        AlertDialog.Builder(this)
+                .setTitle(alertTitle)
+                .setMessage(errorMessage)
+                .setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+                    run {
+                        this@ChatActivity.finish()
+                    }
+                })
+                .show()
+    }
+
+    // activity life cycles
+
+    override fun onPause() {
+        Log.d(logTag, "onPause")
+        super.onPause()
+        session.onPause()
+    }
+
+    override fun onResume() {
+        Log.d(logTag, "onResume")
+        super.onResume()
+        session.onResume()
+    }
+
+    //Session Listener
+
+    override fun onConnected(session: Session?) {
+        Log.i(logTag, "Connected")
+    }
+
+    override fun onDisconnected(session: Session?) {
+        Log.i(logTag, "Disconnected")
+    }
+
+    override fun onError(session: Session?, error: OpentokError?) {
+        Log.i(logTag, "Error")
+    }
+
+    override fun onStreamDropped(session: Session?, stream: Stream?) {
+        Log.i(logTag, "Connected")
+    }
+
+    override fun onStreamReceived(session: Session?, stream: Stream?) {
+        Log.i(logTag, "Connected")
+    }
+
+    // signal listener
+
+    override fun onSignalReceived(session: Session?, type: String?, data: String?, connection: Connection?) {
+        val remote = connection?.equals(session?.connection)?.not()
+
+        if (type != null && type == signalType) {
+            showMessage(data!!, remote!!)
+        }
+    }
+
+
+
 }
